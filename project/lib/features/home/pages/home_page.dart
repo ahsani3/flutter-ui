869f85ca3ee94595c426b1ba345d58412e38de10
ahsani3 <ahsaniweb@gmail.com>
2024-01-31 14:04:09 +0700

    membuat headline aplikasi toko furniture

--- project/lib/features/home/pages/home_page.dart
+++ project/lib/features/home/pages/home_page.dart
@@ -33,6 +33,28 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 14),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 226,
+              child: Text(
+                'Discover the most modern furniture',
+                style: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 22,
+                  color: Color(0xFF4A4543),
+                )),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
